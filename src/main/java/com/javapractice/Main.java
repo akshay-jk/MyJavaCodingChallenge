74795fc152156a5bed7596434820d07ef67fb19f
Akshay JK <akshayjk@zoho.com>
2024-05-25 22:28:44 +0530

    Program to get user input from command line

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -1,7 +1,19 @@
 package com.javapractice;
 
+import java.time.Year;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Daily Practice");
+        //Program to get user input from command line
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter Your Name\t");
+        String UserName = Sc.nextLine();
+        System.out.println("You are " + UserName);
+
+        System.out.print("Enter Your Age\t");
+        int UserAge = Sc.nextInt();
+        int UserBornYear = Year.now().getValue() - UserAge;
+        System.out.println("You were born in " + UserBornYear);
     }
 }
