5b544225e59e9b931a4a4aa01cfce2555801674d
Akshay JK <akshayjk@zoho.com>
2024-05-30 19:50:08 +0530

    Program to check the parity of a number

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -1,19 +1,17 @@
 package com.javapractice;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Program to find the largest number among the given three
-        int LargestNumber = largestFromThree(4, 5, 3);
-        System.out.println("The largest among three is " + LargestNumber);
+        //Program to find the parity of the input
+        Scanner Inp = new Scanner(System.in);
+        System.out.print("Enter the number\t");
+        int NumberToBeChecked = Inp.nextInt();
+        Main.checkParity(NumberToBeChecked);
     }
 
-    public static int largestFromThree(int a, int b, int c) {
-        if (a > b) {
-            if (a > c) return a;
-            else return c;
-        } else {
-            if (b > c) return b;
-            else return c;
-        }
+    public static void checkParity(int num) {
+        System.out.println(num + " is an " + (num % 2 == 0 ? "EVEN" : "ODD") + " number");
     }
 }
