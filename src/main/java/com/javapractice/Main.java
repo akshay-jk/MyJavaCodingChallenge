ea175c13c92ed7e4254b583543f30d18017ef675
Akshay JK <akshayjk@zoho.com>
2024-06-01 00:28:12 +0530

    Program to print prime numbers till N

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -1,5 +1,6 @@
 package com.javapractice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,15 +10,25 @@ public class Main {
         Scanner Inp = new Scanner(System.in);
         System.out.print("Enter the number\t");
 
+        //Obtaining limit from user
         int NumberToBeChecked = Inp.nextInt();
-        System.out.println(NumberToBeChecked + " is " + (isPrimeNumber(NumberToBeChecked) ? "a" : "not") + " prime number");
+
+        if (NumberToBeChecked <= 2) {
+            System.out.println("Lowest prime number is 2");
+        } else {
+            ArrayList<Integer> PrimeNumbers = new ArrayList<>();
+            for (int i = 2; i < NumberToBeChecked; i++) {
+                if (isPrimeNumber(i)) PrimeNumbers.add(i);
+            }
+            System.out.print(PrimeNumbers + " are the prime numbers between 0 and " + NumberToBeChecked);
+        }
     }
 
     //Function that contains the logic
     public static boolean isPrimeNumber(int num) {
-        if (num < 1) return false;
-        for (int i = 1; i < num; i++) {
-            if (i != 1 && num % i == 0) return false;
+        if (num <= 1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
