877414402361ce8d3c81218db9644a0986d8831b
Akshay JK <akshayjk@zoho.com>
2024-06-01 23:05:15 +0530

    Program to find factorial of N

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -1,6 +1,5 @@
 package com.javapractice;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -11,25 +10,29 @@ public class Main {
         System.out.print("Enter the number\t");
 
         //Obtaining limit from user
-        int NumberToBeChecked = Inp.nextInt();
+        int UserInput = Inp.nextInt();
 
-        if (NumberToBeChecked <= 2) {
-            System.out.println("Lowest prime number is 2");
-        } else {
-            ArrayList<Integer> PrimeNumbers = new ArrayList<>();
-            for (int i = 2; i < NumberToBeChecked; i++) {
-                if (isPrimeNumber(i)) PrimeNumbers.add(i);
-            }
-            System.out.print(PrimeNumbers + " are the prime numbers between 0 and " + NumberToBeChecked);
-        }
+        //Obtaining the result using normal function
+        int FactorialValue01 = findFactorial(UserInput);
+        System.out.println(FactorialValue01 + " is the factorial of " + UserInput + ". (Normal Method Computation)");
+
+        //Obtaining the result using recursive function
+        int FactorialValue02 = findFactorialByRecursion(UserInput);
+        System.out.println(FactorialValue02 + " is the factorial of " + UserInput + ". (Recursive Method Computation)");
     }
 
     //Function that contains the logic
-    public static boolean isPrimeNumber(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
+    public static int findFactorial(int num) {
+        if (num == 0 || num == 1) return 1;
+
+        int factorialNum = 1;
+        for (int i = num; i > 1; --i) factorialNum *= i;
+        return factorialNum;
+    }
+
+    //Function that contains the logic using recursion.
+    public static int findFactorialByRecursion(int num) {
+        if (num == 0 || num == 1) return 1;
+        else return num * findFactorialByRecursion(num - 1);
     }
 }
