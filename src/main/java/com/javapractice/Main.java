e0aad19369959fe92acb2326308df8a55025e7f7
Akshay JK <akshayjk@zoho.com>
2024-06-05 19:57:33 +0530

    Program to find palindrome

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -5,30 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner Inp = new Scanner(System.in);
-        System.out.print("Enter the number\t");
+        System.out.print("Enter the text\t");
 
-        //Obtaining limit from user
-        int UserInput = Inp.nextInt();
+        //Obtaining text from user
+        String UserInput = Inp.nextLine();
 
         //Obtaining the result using normal function call
-        System.out.println(UserInput + " is " + (isArmstrongNumber(UserInput) ? "an" : "not an") + " armstrong number");
+        System.out.println("The entered text is " + (isPalindrome(UserInput) ? "a" : "not a") + " palindrome");
 
     }
 
-    //Function to check armstrong number
-    public static boolean isArmstrongNumber(int num) {
-        int inputNumber = num;
-        int aggregatorValue = 0;
-        while (inputNumber != 0) {
-            aggregatorValue = aggregatorValue + (doCube(inputNumber % 10));
-            inputNumber = inputNumber / 10;
+    public static boolean isPalindrome(String Str) {
+        String InpCleaned = Str.replace(" ", "");
+        String InpReveresed = "";
+        for (int i = InpCleaned.length() - 1; i > -1; --i) {
+            InpReveresed = InpReveresed + InpCleaned.charAt(i);
         }
-        if(num == aggregatorValue) return true;
-        return false;
-    }
 
-    //Function that return the cube
-    public static int doCube(int num) {
-        return (int) Math.pow(num, 3);
+        if (InpCleaned.equals(InpReveresed)) return true;
+        return false;
     }
 }
