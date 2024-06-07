3feca81f3408a91998441af4b8e4779df6fb2fba
Akshay JK <akshayjk@zoho.com>
2024-06-07 23:48:28 +0530

    Program to find perimeter and area

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -6,39 +6,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner Inp = new Scanner(System.in);
 
-        System.out.print("Enter the Principle Amount\t");
-        //Obtaining text from user
-        int PrincipleAmount = Inp.nextInt();
+        // Reading length
+        System.out.print("Enter the length\t");
+        int length = Inp.nextInt();
 
-        System.out.print("Enter the Rate\t");
-        int Rate = Inp.nextInt();
+        // Reading breadth
+        System.out.print("Enter the breadth\t");
+        int breadth = Inp.nextInt();
 
-        System.out.print("Enter the Tenure\t");
-        int Tenure = Inp.nextInt();
+        // Checking type of quadrilateral
+        String Quadrilateral = (length == breadth ? "Square" : "Rectangle");
 
-        System.out.println();
-
-        //Obtaining the result using normal function call
-        double SimpleInterest = doCalculateSimpleInterest(PrincipleAmount, Rate, Tenure);
-        double CompoundInterest = doCalculateCompoundInterest(PrincipleAmount, Rate, Tenure);
-
-        System.out.println("Simple Interest : " + SimpleInterest);
-        System.out.println("Total Repayment : " + (SimpleInterest + PrincipleAmount));
-
-        System.out.println();
-
-        System.out.println("Compound Interest : " + CompoundInterest);
-        System.out.println("Total Repayment : " + (CompoundInterest + PrincipleAmount));
-
+        // Printing Area
+        System.out.println("The Area of " + Quadrilateral + " is " + doCalculateArea(length, breadth));
+        // Printing Perimeter
+        System.out.println("The Perimeter of " + Quadrilateral + " is " + doCalculatePerimeter(length, breadth));
     }
 
-    // Function for Calculating Simple Interest
-    public static double doCalculateSimpleInterest(int amount, double rate, int tenure) {
-        return (amount * rate * tenure) / 100;
+    public static int doCalculatePerimeter(int length, int breadth) {
+        return 2 * (length + breadth);
     }
 
-    // Function for Calculating Compound Interest
-    public static double doCalculateCompoundInterest(int amount, double rate, int tenure) {
-        return (amount * Math.pow((1 + ((rate / 100) / 12)), (12 * tenure))) - amount;
+    public static int doCalculateArea(int length, int breadth) {
+        return length * breadth;
     }
 }
