cd63bdb45a1bcb723dad41d959fb9ff298a5291c
Akshay JK <akshayjk@zoho.com>
2024-06-16 23:40:27 +0530

    Program to print natural numbers

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -2,22 +2,17 @@ package com.javapractice;
 
 import java.util.Scanner;
 
-import com.javapractice.SwapNumber;
-
 public class Main {
     public static void main(String[] args) {
         Scanner Inp = new Scanner(System.in);
 
-        System.out.print("Enter the first number\t");
-        int NumOne = Inp.nextInt();
+        System.out.print("Enter the limit\t");
+        int limit = Inp.nextInt();
 
-        System.out.print("Enter the second number\t");
-        int NumTwo = Inp.nextInt();
+        doPrintNaturalNumbers(limit);
+    }
 
-        SwapNumber swapClass = new SwapNumber(NumOne, NumTwo);
-
-        System.out.println("Before swapping, A is " + swapClass.getNumberOne() + ", B is " + swapClass.getNumberTwo());
-        swapClass.doSwapNumbers();
-        System.out.println("After swapping, A is " + swapClass.getNumberOne() + ", B is " + swapClass.getNumberTwo());
+    public static void doPrintNaturalNumbers(int limit) {
+        for (int i = 1; i <= limit; i++) System.out.print(i + " ");
     }
 }
