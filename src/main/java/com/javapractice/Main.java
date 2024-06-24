558a0cc01725cf431f88f077b5aa8a7656360584
Akshay JK <akshayjk@zoho.com>
2024-06-24 23:57:26 +0530

    Program to print pyramid pattern

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -6,16 +6,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner Inp = new Scanner(System.in);
 
-        System.out.print("Enter the limit\t");
-        int limit = Inp.nextInt();
+        System.out.print("Enter the height of pyramid\t");
+        int height = Inp.nextInt();
 
-        printRightTriangle(limit);
+        printPyramid(height);
 
     }
 
-    public static void printRightTriangle(int triangleHeight) {
-        for (int i = 1; i <= triangleHeight; i++) {
-            System.out.println("- ".repeat(triangleHeight - i) + "* ".repeat(i));
+    public static void printPyramid(int pyramidHeight) {
+        int pyramidWidth = ((2 * pyramidHeight) - 1);
+
+        for (int i = 0; i < pyramidHeight; i++) {
+            String pyramidLine = "";
+
+            int hyphenCount = (pyramidWidth / 2) - i, starCount = i;
+
+            pyramidLine += " - ".repeat(hyphenCount);
+            pyramidLine += " * ".repeat(starCount);
+            pyramidLine += " * ";
+            pyramidLine += " * ".repeat(starCount);
+            pyramidLine += " - ".repeat(hyphenCount);
+
+            System.out.println(pyramidLine);
+
         }
+
     }
+
 }
