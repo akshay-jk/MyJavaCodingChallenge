634ff7032e59fa180058506bdec8854cf8095ec7
Akshay JK <akshayjk@zoho.com>
2024-07-03 23:58:10 +0530

    Program to demonstrate Linear Search

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -6,23 +6,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner Inp = new Scanner(System.in);
 
-        System.out.print("Enter the size of square\t");
-        int SquareSize = Inp.nextInt();
+        System.out.print("Enter the size of array\t");
+        int ArrSize = Inp.nextInt();
 
-        printSquare(SquareSize);
-    }
+        int[] Arr = new int[ArrSize];
 
-    public static void printSquare(int SquareSize) {
-        for (int i = 0; i < SquareSize; i++) {
-            String Str = "";
-            for (int j = 0; j < SquareSize; j++) {
-                if (i == 0 || i == SquareSize - 1) {
-                    Str += " * ";
-                } else {
-                    Str = (j == 0 || j == SquareSize - 1) ? Str + " * " : Str + " - ";
-                }
-            }
-            System.out.println(Str);
+        System.out.print("Enter the elements of array\t");
+        for (int i = 0; i < ArrSize; i++) {
+            Arr[i] = Inp.nextInt();
         }
+
+        System.out.print("Enter the key to be searched\t");
+        int SearchKey = Inp.nextInt();
+
+        LinearSearch(Arr, SearchKey);
     }
+
+    public static void LinearSearch(int[] Arr, int SearchKey) {
+        for (int i = 0; i < Arr.length; i++) {
+            if (Arr[i] == SearchKey) {
+                System.out.println("Element found at position " + (i + 1));
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+
+
 }
