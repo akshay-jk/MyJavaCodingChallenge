748af7e49baaf9e69282fefe011193d84b9bd41d
Akshay JK <akshayjk@zoho.com>
2024-07-18 22:22:17 +0530

    Program to demonstrate Merge Sort

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -17,23 +17,61 @@ public class Main {
             Arr[i] = Integer.parseInt(Inp.next());
         }
 
-        InsertionSort(Arr);
+        MergeSort(Arr, 0, Arr.length - 1);
+
+        System.out.println("Array After Sorting");
+        System.out.println(Arrays.toString(Arr));
     }
 
-    public static void InsertionSort(int[] ArrToSort) {
-        for (int i = 1; i < ArrToSort.length; i++) {
-            int ArrPointer = i;
-            while (ArrPointer > 0) {
-                if (ArrToSort[ArrPointer - 1] > ArrToSort[ArrPointer]) {
-                    int SwapVariable = ArrToSort[ArrPointer];
-                    ArrToSort[ArrPointer] = ArrToSort[ArrPointer - 1];
-                    ArrToSort[ArrPointer - 1] = SwapVariable;
-                    ArrPointer = ArrPointer - 1;
-                } else {
-                    break;
-                }
+    public static void MergeSort(int[] UnsortedArr, int StartIndex, int EndIndex) {
+        if (StartIndex < EndIndex) {
+            int MidIndex = (int) Math.floor((EndIndex + StartIndex) / 2);
+
+            MergeSort(UnsortedArr, StartIndex, MidIndex);
+            MergeSort(UnsortedArr, MidIndex + 1, EndIndex);
+            MergeArray(UnsortedArr, StartIndex, MidIndex, EndIndex);
+        }
+    }
+
+    public static void MergeArray(int[] UnsortedArr, int StartIndex, int MidIndex, int EndIndex) {
+        int FirstArrayLen = (MidIndex - StartIndex) + 1;
+        int SecondArrayLen = EndIndex - MidIndex;
+
+        int[] LeftArr = new int[FirstArrayLen];
+        int[] RightArr = new int[SecondArrayLen];
+
+        for (int x = 0; x < FirstArrayLen; x++) {
+            LeftArr[x] = UnsortedArr[StartIndex + x];
+        }
+
+        for (int y = 0; y < SecondArrayLen; y++) {
+            RightArr[y] = UnsortedArr[MidIndex + 1 + y];
+        }
+
+        int i = 0, j = 0, k = StartIndex;
+
+        while (i < FirstArrayLen && j < SecondArrayLen) {
+            if (LeftArr[i] <= RightArr[j]) {
+                UnsortedArr[k] = LeftArr[i];
+                i++;
+            } else {
+                UnsortedArr[k] = RightArr[j];
+                j++;
             }
-            System.out.println(Arrays.toString(ArrToSort));
+            k++;
+
+        }
+
+        while (i < FirstArrayLen) {
+            UnsortedArr[k] = LeftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < SecondArrayLen) {
+            UnsortedArr[k] = RightArr[j];
+            j++;
+            k++;
         }
     }
 }
